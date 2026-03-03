bce6d194e2ad09ae66fb2f8b50e041ee7ebd3315
hugoakerlund <hugo@hugoakerlund.net>
2026-03-03 19:33:22 +0200

    Add table Championship_titles

--- schema.sql
+++ schema.sql
@@ -19,13 +19,18 @@ CREATE TABLE Dogs (
   owner_id INTEGER REFERENCES Users(id),
   picture INTEGER REFERENCES Pictures(id),
   litters INTEGER REFERENCES Litters(id),
-  championship_title TEXT CHECK (championship_title IN ('KVA', 'KV', 'KVA2', 'KV2', 'KVA3', 'KV3', 'KVA4', 'KV4', 'KVA5', 'KV5', 'KVA6', 'KV6', 'KVA7', 'KV7', 'KVA8', 'KV8', 'KVA9', 'KV9')),
+  championship_title INTEGER REFERENCES Championship_titles(id)
   best_test INTEGER CHECK (best_test IN (1, 2, 3, 4, 5)),
   best_show TEXT,
   litter_score TEXT,
   score_trace TEXT,
   hip_index INTEGER,
   use_index INTEGER
+);
+
+CREATE TABLE Championship_titles (
+  id INTEGER PRIMARY KEY,
+  title, TEXT UNIQUE
 );
 
 CREATE TABLE DogBreeds (
