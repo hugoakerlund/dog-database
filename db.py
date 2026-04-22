07579189ae2367d3327b55bbb51bfca47990802b
hugoakerlund <hugo@hugoakerlund.net>
2026-04-22 21:36:48 +0300

    Get rid of dangerous default value.

--- db.py
+++ db.py
@@ -13,7 +13,9 @@ def get_connection():
     con.row_factory = sqlite3.Row
     return con
 
-def execute(sql, params=[]):
+def execute(sql, params=None):
+    if params is None:
+        params = []
     try:
         con = get_connection()
         result = con.execute(sql, params)
@@ -28,7 +30,9 @@ def execute(sql, params=[]):
 def last_insert_id():
     return g.last_insert_id
 
-def query(sql, params=[]):
+def query(sql, params=None):
+    if params is None:
+        params = []
     con = get_connection()
     result = con.execute(sql, params).fetchall()
     con.close()
